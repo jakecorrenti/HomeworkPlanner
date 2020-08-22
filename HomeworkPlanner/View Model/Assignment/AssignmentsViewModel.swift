2574184101c2ad0f6fceba87c515e5c5b2eed3fa
Jake Correnti <jcorrenti13@gmail.com>
2020-08-22 13:33:21 -0400

    added basic segmented filter in the assignments view

--- HomeworkPlanner/View Model/Assignment/AssignmentsViewModel.swift
+++ HomeworkPlanner/View Model/Assignment/AssignmentsViewModel.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Jake Correnti. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
 import CoreData
 
 struct AssignmentsViewModel {
@@ -21,6 +21,21 @@ struct AssignmentsViewModel {
             try context.save()
         } catch {
             print("🔴 Error: \(error.localizedDescription)")
+        }
+    }
+    
+    func filterAssignments(state: Int, data: FetchedResults<Assignment>) -> [Assignment] {
+        let chronologicalSort = data.sorted { $0.dueDate ?? Date() < $1.dueDate ?? Date() }
+        
+        switch state {
+        case 0:
+            return chronologicalSort.filter { !$0.isComplete } + chronologicalSort.filter { $0.isComplete }
+        case 1:
+            return chronologicalSort.filter { !$0.isComplete }
+        case 2:
+            return chronologicalSort.filter { $0.isComplete }
+        default:
+            return chronologicalSort
         }
     }
 }
